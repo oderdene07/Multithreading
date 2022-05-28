fbb300832393326a27ca3897713aee6809b12af3
odko2000 <oderdene07@gmail.com>
2022-05-29 01:07:00 +0800

    Initial import

---
+++ Multithreading/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Multithreading
+{
+    class Program
+    {
+        public static int total = 0;
+        public static void Sum(object filename)
+        {
+            int temp = 0;
+            var data = File.ReadAllText((string)filename);
+
+            var lines = data.Split(new[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in lines)
+            {
+                temp += int.Parse(item);
+            }
+            lock((object)total)
+            {
+                total += temp;
+            }
+        }
+
+        public static void Multithread()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string[] filePaths = Directory.GetFiles(@"data\", "*.txt", SearchOption.TopDirectoryOnly);
+            foreach (string filePath in filePaths)
+            {
+                Thread thread = new Thread(Sum);
+                thread.Start(filePath);
+
+            }
+            Thread.Sleep(100);
+            Console.WriteLine(total);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            Console.WriteLine("RunTime " + ts.Milliseconds);
+        }
+        public static void SingleThread()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string[] filePaths = Directory.GetFiles(@"data\", "*.txt", SearchOption.TopDirectoryOnly);
+            foreach (string filePath in filePaths)
+            {
+                Thread thread = new Thread(Sum);
+                thread.Start(filePath);
+                thread.Join();
+
+            }
+            Thread.Sleep(100);
+            Console.WriteLine(total);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            Console.WriteLine("RunTime " + ts.Milliseconds);
+        }
+
+        public static void Main()
+        {
+            Multithread();
+            total = 0;
+            SingleThread();
+        }
+    }
+}
---
+++ Multithreading/ProgramTest.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Threading;
+
+//namespace Multithreading
+//{
+//    class ProgramTest
+//    {
+//        public static int res = 0;
+//        public static void DoWork(object filePath)
+//        {
+//            int temp = 0;
+
+//            var data = File.ReadAllText((string)filePath);
+//            var lines = data.Split(new[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+//            foreach(var line in lines)
+//            {
+//                temp += int.Parse(line);
+//            }
+//            lock ((object)res)
+//            {
+//                res += temp;
+//            }
+//        }
+
+//        public static void Main()
+//        {
+//            string[] filePaths = Directory.GetFiles(@"data\", "*.txt", SearchOption.TopDirectoryOnly);
+//            Thread th1 = new Thread(DoWork);
+//            th1.Start(filePaths[0]);
+
+//            Thread th2 = new Thread(DoWork);
+//            th2.Start(filePaths[1]);
+
+//            Thread th3 = new Thread(DoWork);
+//            th3.Start(filePaths[2]);
+            
+//            Thread th4 = new Thread(DoWork);
+//            th4.Start(filePaths[3]);
+
+
+//            //Thread.Sleep(100);
+            
+//            while (true)
+//            {
+//                if(!th1.IsAlive && !th2.IsAlive && !th3.IsAlive && !th4.IsAlive)
+//                {
+//                    Console.WriteLine(res);
+//                    break;
+//                }
+//                else
+//                {
+//                    continue;
+//                }
+//            }
+            
+//        }
+//    }
+//}
